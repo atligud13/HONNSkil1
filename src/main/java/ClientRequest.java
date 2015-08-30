081b61a6cdff8c66273167779420bcd0bb26bbd7
Atli <atligud13@ru.is>
2015-08-30 13:37:09 +0000

    created the teams project

---
+++ src/main/java/ClientRequest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Atli Guðlaugsson on 30/08/2015.
+ */
+public class ClientRequest {
+    public static void main(String[] argv) {
+        System.out.println("Hehe");
+    }
+
+    public String getFileContent(String fileName) throws RequestException{
+
+        return null;
+    }
+}
---
+++ src/main/java/RequestException.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Atli Guðlaugsson on 30/08/2015.
+ */
+public class RequestException extends Exception {
+    public RequestException(String message) { super(message); }
+}
